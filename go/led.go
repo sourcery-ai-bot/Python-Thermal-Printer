990f7ec89012c10a005d57d78f79ec35c7dc6165
Stan Rozenraukh <stan@stanistan.com>
2021-01-10 11:52:13 -0500

    some moving things around

--- go/led.go
+++ go/led.go
@@ -11,13 +11,17 @@ import (
 )
 
 type LEDPin struct {
-	pin   gpio.PinIO
-	level gpio.Level
+	pin    gpio.PinIO
+	level  gpio.Level
+	ticker *ledTickerState
 }
 
 func NewLEDPin() *LEDPin {
 	return &LEDPin{
 		pin: gpioreg.ByName(LEDPinName),
+		ticker: &ledTickerState{
+			stopped: true,
+		},
 	}
 }
 
@@ -38,36 +42,37 @@ func (p *LEDPin) Out(l gpio.Level) error {
 	return p.pin.Out(l)
 }
 
+func (p *LEDPin) Handle(e *Event) error {
+	switch e.Kind {
+	case ButtonPressed:
+		p.ticker.Stop()
+		return p.On()
+	case ButtonReleased:
+		return p.Off()
+	case Idle:
+		p.ticker.Start()
+	}
+	return nil
+}
+
 func (p *LEDPin) ToggleLoop(ctx context.Context, wg *sync.WaitGroup, events <-chan *Event) {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		stopped := false
-		t := ticker()
+		p.ticker.Start()
 		for {
 			select {
 			case e := <-events:
-				// we send a copy of the events here,
-				// and this only handles the press, release, and idle.
-				switch e.Kind {
-				case ButtonPressed:
-					t.Stop()
-					stopped = true
-					_ = p.On()
-				case ButtonReleased:
-					_ = p.Off()
-				case Idle:
-					if stopped {
-						t = ticker()
-						stopped = false
-					}
+				if err := p.Handle(e); err != nil {
+					log.Printf("[Error] %s", err)
+					return
 				}
 			case <-ctx.Done():
 				if err := p.Off(); err != nil {
 					log.Printf("[Error] %s", err)
 				}
 				return
-			case <-t.C:
+			case <-p.ticker.t.C:
 				if err := p.Toggle(); err != nil {
 					log.Printf("[Error] %s", err)
 					return
@@ -77,6 +82,19 @@ func (p *LEDPin) ToggleLoop(ctx context.Context, wg *sync.WaitGroup, events <-ch
 	}()
 }
 
-func ticker() *time.Ticker {
-	return time.NewTicker(500 * time.Millisecond)
+type ledTickerState struct {
+	t       *time.Ticker
+	stopped bool
+}
+
+func (t *ledTickerState) Start() {
+	if t.stopped {
+		t.stopped = false
+		t.t = time.NewTicker(500 * time.Millisecond)
+	}
+}
+
+func (t *ledTickerState) Stop() {
+	t.stopped = true
+	t.t.Stop()
 }
